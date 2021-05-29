020f71e8d3c4088bc49091bba860585bfddfb2b1
sm-yunis <salmanmohammed176@gmail.com>
2021-05-29 12:41:50 +0300

    terminating middleware moved to program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace EchoServer
 {
@@ -20,7 +19,42 @@ namespace EchoServer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(new string[] { "http://127.0.0.1:5000" });
+                    webBuilder.Configure(app =>
+                    {
+
+                        app.Run(async context =>
+                        {
+
+                            await context.Response.WriteAsync("************************\n");
+                            await context.Response.WriteAsync($"Method :  {context.Request.Method.ToUpper()} \n");
+                            await context.Response.WriteAsync($"Path :  {context.Request.GetEncodedPathAndQuery() } \n");
+
+
+                            await context.Response.WriteAsync("\n******* Headers ********\n");
+                            foreach (var header in context.Request.Headers)
+                            {
+                                await context.Response.WriteAsync($"{header.Key} : {header.Value} \n");
+                            }
+
+                            await context.Response.WriteAsync("\n********* Body *********\n");
+
+                            string requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
+                            await context.Response.WriteAsync(requestBody);
+
+
+                            await context.Response.WriteAsync("\n********* Time *********\n");
+                            await context.Response.WriteAsync(DateTime.Now.ToString());
+                            await context.Response.WriteAsync("\n\n************************\n");
+
+
+                        });
+
+                    });
+
                 });
+
+
+
     }
 }
